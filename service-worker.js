0dff50e6d6ba4b86c9e9e7f77afe39dcf260c29c
Alec Resende <universalsequences@gmail.com>
2023-09-24 00:18:45 -0400

    fuck

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65d149390a9597630dd3baa4f66cfc09.js"
+  "/precache-manifest.f0b287c37239eb858f47367ca494fca8.js"
 );
 
 workbox.clientsClaim();
